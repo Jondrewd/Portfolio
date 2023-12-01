1fd4d18c07c4bad56e60f4a4105f12fac6d4381c
Jondrewd <jonas220703@gmail.com>
2023-12-01 19:42:30 -0300

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+import Home from './components/pages/Home';
+import Skills from './components/pages/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <Link to='/'> Home</Link>
+        <Link to='/skills'> Skills</Link>
+      </ul>
+      <Routes>
+        <Route path='/' exact element={<Home/>}/>
+        <Route path='/skills' exact element={<Skills/>}/>
+      </Routes>
+    </Router>
   );
 }
 
