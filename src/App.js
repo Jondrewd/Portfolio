6b0559b056747e3597e74f8f6847ba3c7f8b50eb
Jondrewd <jonas220703@gmail.com>
2023-12-02 21:08:57 -0300

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
+import './App.css'
 import Home from './components/pages/Home';
 import Skills from './components/pages/Skills';
+import Navbar from './components/layout/Navbar';
+import Aboutme from './components/pages/Aboutme';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
-    <Router>
-      <ul>
-        <Link to='/'> Home</Link>
-        <Link to='/skills'> Skills</Link>
-      </ul>
-      <Routes>
-        <Route path='/' exact element={<Home/>}/>
-        <Route path='/skills' exact element={<Skills/>}/>
-      </Routes>
-    </Router>
+    <body>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/skills' exact element={<Skills/>}/>
+          <Route path='/aboutme' exact element={<Aboutme/>}/>
+          <Route path='/projects' exact element={<Projects/>}/>
+        </Routes>
+      </Router>
+    </body>
   );
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,18 @@
+import {Link} from 'react-router-dom'
+
+import styles from './Navbar.module.css'
+
 function Navbar(){
     return(
-    <h1>Navbar</h1>
+        <div className={styles.nav_layout}>
+            <h1>Portfolio</h1>
+            <ul>
+                <Link className={styles.nav_link} to='/'> Home</Link>
+                <Link className={styles.nav_link} to='/skills'> Skills</Link>
+                <Link className={styles.nav_link} to='/aboutme'> About me</Link>
+                <Link className={styles.nav_link} to='/projects'> Projects</Link>
+            </ul>
+        </div>
 )}
 
 export default Navbar
