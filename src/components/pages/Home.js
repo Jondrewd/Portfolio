727a28e7adaf8f3e69dbb5aa5be677a7df2859da
Jondrewd <jonas220703@gmail.com>
2023-12-03 18:15:41 -0300

    Portfolio home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,20 @@
+import styles from './Home.module.css'
+
+import { FaSquareGithub, FaLinkedin } from "react-icons/fa6";
+
 function Home(){
     return(
-    <h1>Home</h1>
+        <div className={styles.welcome_container}>
+        <h2>Welcome!</h2>
+        <div className={styles.text_container}>
+            <span>I'm a </span>
+            <span>Fullstack </span>
+            <span>Developer</span>
+        </div>
+        <span className={styles.social_media}>
+            <a href="https://github.com/Jondrewd" target='_blank'><FaLinkedin className={styles.media_a}/></a> 
+            <a href="" target='_blank'><FaSquareGithub className={styles.media_a}/></a></span>
+        </div>
 )}
 
 export default Home
