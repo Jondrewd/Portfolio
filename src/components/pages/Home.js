5950f33630880e372efdbc0d151afbe2b37970bd
Jondrewd <jonas220703@gmail.com>
2023-12-11 17:32:26 -0300

    Update home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,14 @@ import { FaSquareGithub, FaLinkedin } from "react-icons/fa6";
 
 function Home(){
     return(
-   <h1>Home</h1>
+        <div className={styles.homeContainer}>
+            <div className={styles.me}></div>
+            <div className={styles.textHome}>
+                <h2>Hello world!</h2>
+                <h1>oi,</h1>
+                <h2>Desenvolvedor Back-end.</h2>
+            </div>
+        </div>
 )}
 
 export default Home
