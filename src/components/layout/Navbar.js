51d195a2fefb8294c15c1abd1ca5b4a5b77136fe
Jondrewd <jonas220703@gmail.com>
2023-12-29 18:09:07 -0300

    Update Navbar.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,21 @@
 import {Link} from 'react-router-dom'
 import { GiAbstract050 } from "react-icons/gi";
 import styles from './Navbar.module.css'
-
+import { Sidebar } from './Sidebar';
 function Navbar(){
 
 
     return(
         <div className={styles.nav_layout}>
-            <h1>Portfolio</h1>
+           <Link className={styles.nav_linkh1} to='/'>  <h1>Portfolio</h1></Link>
             <ul>
                 <Link className={styles.nav_link} to='/'> Home</Link>
                 <Link className={styles.nav_link} to='/skills'> Skills</Link>
                 <Link className={styles.nav_link} to='/aboutme'> About me</Link>
                 <Link className={styles.nav_link} to='/projects'> Projects</Link>
             </ul>
-          <div className={styles.jedi}></div>
+            
+          <div className={styles.jedi}></div>  <Sidebar/>
         </div>
         
 )}
