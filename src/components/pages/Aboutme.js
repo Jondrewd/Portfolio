6b4b64541e8c70c6d506aec1e930f7adbe80a045
Jondrewd <jonas220703@gmail.com>
2024-01-02 18:41:13 -0300

    update

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -3,7 +3,14 @@ import styles from './Aboutme.module.css'
 function Aboutme(){
     return(
     <div className={styles.aboutLayout}>
-        <h1>Aboutme</h1>
+        <nav className={styles.imageA}> </nav>
+        <nav className={styles.textA}>
+            <h1>Olá, eu sou Jonas,</h1> 
+            <p>um estudante de <strong>Ciências da Computação</strong> fascinado por tecnologia e inovação. Eu tenho experiência em desenvolvimento web com <strong>React</strong> e <strong>Java</strong>, e estou sempre buscando aprender novas ferramentas e linguagens.</p>
+             <p>No meu portfolio, você vai encontrar alguns dos meus projetos pessoais e acadêmicos, que demonstram as minhas habilidades e interesses. Eu espero que você goste do meu trabalho e entre em contato comigo se tiver alguma dúvida ou sugestão.</p>
+
+            <p><strong>Obrigado pela visita!</strong></p>
+        </nav>
     </div>
 )}
 
