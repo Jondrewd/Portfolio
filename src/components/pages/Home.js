1b69d709e93450a2ec88b889cd870e008d816bd5
Jondrewd <jonas220703@gmail.com>
2024-01-02 18:41:23 -0300

    update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ import { FaSquareGithub, FaLinkedin } from "react-icons/fa6";
 function Home(){
     return(
    <div className={styles.homeLayout}>
-       <div className={styles.text}>
+       <div className={styles.textH}>
            <h1>Desenvolvedor <strong>fullstack</strong></h1>
            <p>Crio sites utilizando React e Java</p>
            <Button to="/contato" text="Entre em contato" />
