565e56bb6bf215d67841bb4869737eaa8f7ca1b2
Jondrewd <jonas220703@gmail.com>
2024-01-03 17:53:34 -0300

    Update Skills.js

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -14,7 +14,9 @@ function Skills(){
                     <FaReact className={styles.icons}/>
                     <h1>React</h1>
                 </nav>
-                <p>O <strong>React</strong> é uma biblioteca front-end <strong>JavaScript</strong> de código aberto com foco em criar interfaces de usuário em páginas web.</p>
+                <nav className={styles.textL}>
+                    <p className={styles.descP}>O <strong>React</strong> é uma biblioteca front-end <strong>JavaScript</strong> de código aberto com foco em criar interfaces de usuário em páginas web.</p>
+                </nav>
                 </div>
 
             <div className={styles.linguagemlt}>
@@ -22,7 +24,9 @@ function Skills(){
                     <IoLogoJavascript className={styles.icons}/>
                     <h1>Javascript</h1>
                 </nav>
-                <p>O <strong>Javascript</strong> é linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.</p>
+                <nav className={styles.textL}>
+                    <p className={styles.descP}>O <strong>Javascript</strong> é linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.</p>
+                </nav>
                 </div>
 
             <div className={styles.linguagemlt}>
@@ -30,7 +34,9 @@ function Skills(){
                     <FaHtml5 className={styles.icons}/>
                     <h1>HTML</h1>
                 </nav>
-                <p><strong>HTML</strong> é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos <strong>HTML</strong> podem ser interpretados por navegadores.</p>
+                <nav className={styles.textL}>
+                    <p className={styles.descP}><strong>HTML</strong> é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos <strong>HTML</strong> podem ser interpretados por navegadores.</p>
+                </nav>
             </div>
 
             <div className={styles.linguagemlt}>
@@ -38,14 +44,18 @@ function Skills(){
                     <FaCss3 className={styles.icons}/>
                     <h1>CSS</h1>
                 </nav>
-                <p><strong>Css</strong> é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags <strong>HTML</strong>. </p>
+                <nav className={styles.textL}>
+                    <p className={styles.descP}><strong>Css</strong> é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags <strong>HTML</strong>. </p>
+                </nav>
             </div>
             <div className={styles.linguagemlt}>
                 <nav className={styles.textL}>
                     <FaJava className={styles.icons}/>
                     <h1>Java</h1>
                 </nav>
-                <p><strong>Java</strong> é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores na empresa Sun Microsystems.</p>
+                <nav className={styles.textL}>
+                    <p className={styles.descP}><strong>Java</strong> é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores na empresa Sun Microsystems.</p>
+                </nav>
             </div>
 
         </nav>
@@ -58,7 +68,9 @@ function Skills(){
                     <FaPython className={styles.icons}/>
                     <h1>Python</h1>
                 </nav>
-                <p><strong>Python</strong> é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.</p>
+                <nav className={styles.textL}>
+                    <p className={styles.descP}><strong>Python</strong> é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.</p>
+                </nav>
                 </div>
         </nav>
         
