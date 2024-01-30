061d7793baf89e4738ad229e681b3c414481be83
Jondrewd <jonas220703@gmail.com>
2024-01-30 17:37:02 -0300

    Update Skills.js

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -7,7 +7,7 @@ import styles from './Skills.module.css'
 function Skills(){
     return(
     <div className={styles.skillsLayout}>
-        <h1>Conhecimentos sólidos em:</h1>
+        <h1>Tecnologias e Habilidads</h1>
         <nav className={styles.frontback}>
             <div className={styles.linguagemlt}>
                 <nav className={styles.textL}>
@@ -35,44 +35,11 @@ function Skills(){
                     <FaHtml5 className={styles.icons}/>
                     <h1>HTML</h1>
                 </nav>
-                <nav className={styles.textL}>
-                    <p className={styles.descP}><strong>HTML</strong> é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos <strong>HTML</strong> podem ser interpretados por navegadores.</p>
-                </nav>
-            </div>
-
-            <div className={styles.linguagemlt}>
-                <nav className={styles.textL}>
-                    <FaCss3 className={styles.icons}/>
-                    <h1>CSS</h1>
-                </nav>
-                <nav className={styles.textL}>
-                    <p className={styles.descP}><strong>Css</strong> é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags <strong>HTML</strong>. </p>
-                </nav>
-            </div>
-            <div className={styles.linguagemlt}>
-                <nav className={styles.textL}>
-                    <FaJava className={styles.icons}/>
-                    <h1>Java</h1>
-                </nav>
-                <nav className={styles.textL}>
-                    <p className={styles.descP}><strong>Java</strong> é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores na empresa Sun Microsystems.</p>
-                </nav>
             </div>
         </nav>
 
-        <h1>Aprendendo:</h1>
+    
 
-        <nav className={styles.frontback}>
-            <div className={styles.linguagemlt}>
-                <nav className={styles.textL}>
-                    <FaPython className={styles.icons}/>
-                    <h1>Python</h1>
-                </nav>
-                <nav className={styles.textL}>
-                    <p className={styles.descP}><strong>Python</strong> é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.</p>
-                </nav>
-            </div>
-        </nav>
     </div>
 )}
 
